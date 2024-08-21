f9aa9dbdf43f78b3b5f8f1503366592018005c8c
Vinicius Lazzari <vininet17@gmail.com>
2024-08-20 22:49:31 -0300

    ajustado porta para 5173

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    port: 5173
   },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   }
-})
+});
