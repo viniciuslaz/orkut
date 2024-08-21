36299c0ac0ce7df396ef2ebf3fd68365eabfaf49
Vinicius Lazzari <vininet17@gmail.com>
2024-08-20 22:50:18 -0300

    adicionado rota / para pagina main e not-found para /login

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/login/index.login.vue')
+      path: "/",
+      name: "main",
+      component: () => import("../views/main/index.main.vue")
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/login/index.login.vue")
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      redirect: () => {
+        return { path: "/login" };
+      }
     }
   ]
-})
+});
 
-export default router
+export default router;
